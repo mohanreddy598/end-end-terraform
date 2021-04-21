90fb72271007805b69ad6f725fdd783f28271305
mohanreddy598 <82871901+mohanreddy598@users.noreply.github.com>
2021-04-21 14:58:23 +0530

    Update ecs.tf

--- devops/ecs.tf
+++ devops/ecs.tf
@@ -1,9 +1,11 @@
 resource "aws_ecs_cluster" "main" {
   name = "jms-cluster"
 }
+
 data "aws_ecr_repository" "my-first-repo" {
 name = "nodeproj"
 }
+
 data "template_file" "cb_app" {
   template = file("./templates/ecs/cb_app.json.tpl")
 
